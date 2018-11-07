b55b0af2fcc793124e85f842347288bdbda84b1e
Zagoret Sergiu <zagoret@outlook.com>
2018-11-07 21:01:30 +0200

    Add heroku remote cors

--- src/NotesPage.react.js
+++ src/NotesPage.react.js
@@ -3,48 +3,15 @@
 import React from "react";
 
 import PageWrapper from "./components/PageWrapper.react";
-import {
-  Page,
-  Grid,
-  StatsCard,
-  PricingCard,
-  Icon
-}from "tabler-react";
+
+import FolderComponent from "./components/Folder.react";
+import {Page}from "tabler-react";
 
 function NotesPage() {
   return(
         <PageWrapper>
           <Page.Content>
-            <Grid.Row cards={true}>
-              <Grid.Col width={6} sm={4} lg={2}>
-                <PricingCard>
-                  <PricingCard.Category>{"Orchestra"}</PricingCard.Category>
-                  <PricingCard.Price><Icon name="folder" /></PricingCard.Price>
-                  <PricingCard.Button> {"Orchestra"} </PricingCard.Button>
-                </PricingCard>
-              </Grid.Col>
-              <Grid.Col width={6} sm={4} lg={2}>
-                <PricingCard>
-                  <PricingCard.Category>{"Orchestra"}</PricingCard.Category>
-                  <PricingCard.Price><Icon name="folder" /></PricingCard.Price>
-                  <PricingCard.Button> {"Orchestra"} </PricingCard.Button>
-                </PricingCard>
-              </Grid.Col>
-              <Grid.Col width={6} sm={4} lg={2}>
-                <PricingCard>
-                  <PricingCard.Category>{"Orchestra"}</PricingCard.Category>
-                  <PricingCard.Price><Icon name="folder" /></PricingCard.Price>
-                  <PricingCard.Button> {"Orchestra"} </PricingCard.Button>
-                </PricingCard>
-              </Grid.Col>
-              <Grid.Col width={6} sm={4} lg={2}>
-                <PricingCard>
-                  <PricingCard.Category>{"Orchestra"}</PricingCard.Category>
-                  <PricingCard.Price><Icon name="folder" /></PricingCard.Price>
-                  <PricingCard.Button> {"Orchestra"} </PricingCard.Button>
-                </PricingCard>
-              </Grid.Col>
-            </Grid.Row>
+            <FolderComponent/>
           </Page.Content>
         </PageWrapper>
   );
---
+++ src/components/Folder.react.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import {Grid, Icon, PricingCard} from "tabler-react";
+
+export default class FolderComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://daswort-api.herokuapp.com//files")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        })
+  }
+
+  render() {
+    const {error, isLoaded, items} = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <Grid.Row cards={true}>
+          {items.map(item => (
+            <Grid.Col width={6} sm={4} lg={2}>
+              <PricingCard>
+                <PricingCard.Category>{item.name}</PricingCard.Category>
+                <PricingCard.Price><Icon name="folder"/></PricingCard.Price>
+                <PricingCard.Button> {"Orchestra"} </PricingCard.Button>
+              </PricingCard>
+            </Grid.Col>
+          ))}
+        </Grid.Row>
+      );
+    }
+  }
+}
