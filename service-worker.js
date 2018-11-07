ade03db71957b627404a68604c17198ede052c91
Zagoret Sergiu <zagoret@outlook.com>
2018-11-07 21:02:12 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/daswort/precache-manifest.b93f353d8adfb4a9318ab5aef6c05135.js"
+  "/daswort/precache-manifest.a1a77b9362e0b52e161c7a569853c0b5.js"
 );
 
 workbox.clientsClaim();
